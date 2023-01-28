c6649216d86b2d75967c0bcad2b2e88e8349db7d
ubaidubai <avez3npqureshi@gmail.com>
2023-01-28 22:24:11 +0530

    netlify issue solved

--- server/index.js
+++ server/index.js
@@ -25,8 +25,8 @@ app.post("/checkout", async (req, res) => {
     const session = await stripe.checkout.sessions.create({
         line_items: lineItems,
         mode: 'payment',
-        success_url: "https://ecom-avez.vercel.app/success",
-        cancel_url: "https://ecom-avez.vercel.app/cancel"
+        success_url: "https://ecom-avez.netlify.app/success",
+        cancel_url: "https://ecom-avez.netlify.app/cancel"
     });
 
     res.send(JSON.stringify({
